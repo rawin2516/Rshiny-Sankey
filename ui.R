32a922e45a27f19c95a879ddf9092bdf28451497
Awin Salian <awin.06g@gmail.com>
2018-02-26 14:01:34 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -4,7 +4,8 @@ library(sankeyD3)
 shinyUI(navbarPage("Sankey Testing by Awin",
 tabPanel("Data Import",
                         sidebarLayout(sidebarPanel( fileInput("file","Upload your CSV",multiple = FALSE),
-                                                    tags$hr(),
+                                                    tags$hr()
+                                                   ,
                                                     h5(helpText("Select the read.table parameters below")),
                                                     checkboxInput(inputId = 'header', label = 'Header', value = FALSE),
                                                     checkboxInput(inputId = "stringAsFactors", "stringAsFactors", FALSE),
@@ -70,46 +71,3 @@ tabPanel("Data Import",
 )
 
 
-shinyServer(function(input, output) {
-    
-  output$sankey <- renderSankeyNetwork({
-    URL <- "https://cdn.rawgit.com/christophergandrud/networkD3/master/JSONdata/energy.json"
-    Energy <- jsonlite::fromJSON(URL)
-    Energy$links$source_name <- Energy$nodes[Energy$links$source+1, "name"]
-    Energy$links$target_name <- Energy$nodes[Energy$links$target+1, "name"]
-    sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
-                  Target = "target", Value = "value", NodeID = "name",
-                  fontSize = 12, 
-                  zoom = input$zoom, align = input$align,
-                  scaleNodeBreadthsByString = input$scaleNodeBreadthsByString,
-                  nodeWidth = input$nodeWidth,
-                  nodeShadow = input$nodeShadow,
-                  linkGradient = input$linkGradient,
-                  linkOpacity = input$linkOpacity,
-                  nodeLabelMargin = input$nodeLabelMargin,
-                  nodeStrokeWidth = input$nodeStrokeWidth,
-                  LinkGroup = ifelse(input$LinkGroup == "none", NA, input$LinkGroup),
-                  NodeGroup = ifelse(input$NodeGroup == "none", NA, input$NodeGroup),
-                  nodePadding = input$nodePadding,
-                  nodeCornerRadius = input$nodeCornerRadius,
-                  showNodeValues = input$showNodeValues,
-                  dragX = input$dragX,
-                  dragY = input$dragY,
-                  linkType = input$linkType,
-                  curvature = input$curvature,
-                  numberFormat = input$numberFormat,
-                  highlightChildLinks = input$highlightChildLinks,
-                  doubleclickTogglesChildren = input$doubleclickTogglesChildren,
-                  orderByPath = input$orderByPath,
-                  xScalingFactor = input$xScalingFactor,
-                  units = "kWh")
-  })
-  
-  output$clicked_node <- renderPrint( {
-    input$sankey_clicked 
-    })
-  output$hovered_node <- renderPrint( {
-    input$sankey_hover
-  })
-  
-})
